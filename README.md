9a82b30d5142df3a4e81145419ccd3a105d39dc2
Arath Astorga <56246139+Arselt@users.noreply.github.com>
2021-11-01 22:21:52 -0600

    ✏ fix typos on README, Routes and 404

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # cientifico
-"Curso de Single Page Application con JavaScript Vanilla" from Platzi teached by @gndx
+"Curso de Single Page Application con JavaScript Vanilla" from Platzi taught by @gndx
--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -2,7 +2,7 @@ const  Error404 = () => {
     const view = `
         <div class="Error404">
             <h2>
-                Error 4040
+                Error 404
             </h2>
         </div>
     `;
